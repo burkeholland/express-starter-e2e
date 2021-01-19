e7b3c5573e6a7a4492d123e2689a6655ab282a42
Burke Holland <burkeholland@gmail.com>
2021-01-19 18:14:10 +0000

    modify title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Codespace World!' });
+  res.render('index', { title: 'Hello Codespaces!' });
 });
 
 module.exports = router;
